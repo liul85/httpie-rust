041a6361bcce6e497548e418e4669e87df8baf7c
liul85 <liul85@gmail.com>
2021-09-01 20:19:41 +1000

    send http request

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::{anyhow, Result};
 use clap::{AppSettings, Clap};
-use reqwest::Url;
+use reqwest::{Client, Url};
+use std::collections::HashMap;
 use std::str::FromStr;
 
 #[derive(Clap, Debug)]
@@ -61,7 +62,30 @@ fn parse_kv_pair(s: &str) -> Result<KvPair> {
     Ok(s.parse()?)
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     let opts = Opts::parse();
-    println!("{:?}", opts);
+    let client = Client::new();
+    let result = match opts.subcmd {
+        SubCommand::Get(args) => get(client, &args).await?,
+        SubCommand::Post(args) => post(client, &args).await?,
+    };
+    Ok(result)
+}
+
+async fn get(client: Client, args: &Get) -> Result<()> {
+    let response = client.get(&args.url).send().await?;
+    println!("{:?}", response.text().await?);
+    Ok(())
+}
+
+async fn post(client: Client, args: &Post) -> Result<()> {
+    let mut body = HashMap::new();
+    for kv in args.body.iter() {
+        body.insert(&kv.key, &kv.value);
+    }
+
+    let response = client.post(&args.url).json(&body).send().await?;
+    println!("{:?}", response.text().await?);
+    Ok(())
 }
