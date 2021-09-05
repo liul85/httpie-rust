2e0c632a006321072534a6e84b3943b3879ce67c
liul85 <liul85@gmail.com>
2021-09-05 14:10:32 +1000

    highlight json body

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ use mime::Mime;
 use reqwest::{header, Client, Response, Url};
 use std::collections::HashMap;
 use std::str::FromStr;
+use syntect::easy::HighlightLines;
+use syntect::highlighting::{Style, ThemeSet};
+use syntect::parsing::SyntaxSet;
+use syntect::util::{as_24_bit_terminal_escaped, LinesWithEndings};
 
 #[derive(Clap, Debug)]
 #[clap(setting = AppSettings::ColoredHelp)]
@@ -68,11 +72,11 @@ fn parse_kv_pair(s: &str) -> Result<KvPair> {
 async fn main() -> Result<()> {
     let opts = Opts::parse();
     let client = Client::new();
-    let result = match opts.subcmd {
+    match opts.subcmd {
         SubCommand::Get(args) => get(client, &args).await?,
         SubCommand::Post(args) => post(client, &args).await?,
     };
-    Ok(result)
+    Ok(())
 }
 
 async fn get(client: Client, args: &Get) -> Result<()> {
@@ -98,7 +102,7 @@ async fn print_resp(response: Response) -> Result<()> {
 
     let mime = get_content_type(&response);
     let body = response.text().await?;
-    print_body(mime, body);
+    print_body(mime, &body);
     Ok(())
 }
 
@@ -122,11 +126,21 @@ fn get_content_type(response: &Response) -> Option<Mime> {
         .map(|v| v.to_str().unwrap().parse().unwrap())
 }
 
-fn print_body(mime: Option<Mime>, body: String) {
+fn print_body(mime: Option<Mime>, body: &str) {
     match mime {
-        Some(v) if v == mime::APPLICATION_JSON => {
-            println!("{}", jsonxf::pretty_print(&body).unwrap().cyan())
-        }
+        Some(v) if v == mime::APPLICATION_JSON => print_syntect(body, "json"),
         _ => println!("{}", body),
     };
+}
+
+fn print_syntect(s: &str, ext: &str) {
+    let ps = SyntaxSet::load_defaults_newlines();
+    let ts = ThemeSet::load_defaults();
+    let syntax = ps.find_syntax_by_extension(ext).unwrap();
+    let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
+    for line in LinesWithEndings::from(&s) {
+        let ranges: Vec<(Style, &str)> = h.highlight(line, &ps);
+        let escaped = as_24_bit_terminal_escaped(&ranges[..], true);
+        print!("{}", escaped);
+    }
 }
